c410845c2f94c896177c1499b5fa9156fecbef2f
Yota Toyama <raviqqe@gmail.com>
2025-08-14 22:42:04 -0700

    Fix

--- examples/shiki/src/index.ts
+++ examples/shiki/src/index.ts
@@ -2,12 +2,15 @@ import { initialize } from "monza-editor";
 import "monza-editor/style.css";
 import "./style.css";
 import { createHighlighter } from "shiki";
+import styles from "./index.module.css";
 
 const container = document.querySelector<HTMLDivElement>("#app");
 
 if (!container) {
   throw new Error("No container");
 }
+
+container.className = styles.editor ?? "";
 
 const highlighter = await createHighlighter({
   langs: ["scheme"],
