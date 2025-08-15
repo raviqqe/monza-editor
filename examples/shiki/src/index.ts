5c7af4780a69d9b82a6698922fdc7c51f08cb544
Yota Toyama <raviqqe@gmail.com>
2025-08-14 22:27:54 -0700

    fix

--- examples/shiki/src/index.ts
+++ examples/shiki/src/index.ts
@@ -1,7 +1,7 @@
 import { initialize } from "monza-editor";
 import "monza-editor/style.css";
 import "./style.css";
-import { codeToHtml } from "shiki";
+import { createHighlighter } from "shiki";
 
 const container = document.querySelector<HTMLDivElement>("#app");
 
@@ -9,8 +9,13 @@ if (!container) {
   throw new Error("No container");
 }
 
+const highlighter = await createHighlighter({
+  langs: ["scheme"],
+  themes: ["vitesse-light"],
+});
+
 initialize(container.appendChild(document.createElement("div")), (text) =>
-  codeToHtml(text, {
+  highlighter.codeToHtml(text, {
     lang: "scheme",
     structure: "inline",
     theme: "vitesse-light",
