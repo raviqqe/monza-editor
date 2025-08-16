e7d81c04ad85f9d8fdccf38933dd9e80db0770a7
Yota Toyama <raviqqe@gmail.com>
2025-08-16 13:01:40 -0700

    fix

--- doc/src/utility.ts
+++ doc/src/utility.ts
@@ -1,6 +1,16 @@
 export const source = `
 import { exit } from "node:process";
 
+const fibonacci = (n: number): number => {
+  if (n <= 1) {
+    return n;
+  }
+
+  return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
 console.log("Hello, world!");
+console.log(fibonacci(32));
+
 exit(0);
 `.trim();
