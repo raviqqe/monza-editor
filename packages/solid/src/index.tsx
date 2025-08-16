a30ac8b8211dbdec56f9a669679d507208871d90
Yota Toyama <raviqqe@gmail.com>
2025-08-15 19:21:22 -0700

    Fix

--- packages/solid/src/index.tsx
+++ packages/solid/src/index.tsx
@@ -1,5 +1,19 @@
-import type { JSX } from "solid-js";
+import { initialize } from "monza-editor";
+import { onMount, type JSX } from "solid-js";
 
-export const Editor = (): JSX.Element => {
-  return <></>;
+interface Props {
+  highlight?: (text: string) => string;
+}
+
+export const Editor = (props: Props): JSX.Element => {
+  let ref;
+
+  onMount(() => {
+    if (!ref) {
+      return;
+    }
+    initialize(ref, props.highlight);
+  });
+
+  return <div ref={ref} />;
 };
