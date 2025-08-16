e9b2bbdf0097f1685635d6c85db07ea6810d16d0
Yota Toyama <raviqqe@gmail.com>
2025-08-16 15:11:46 -0700

    Fix

--- doc/astro.config.ts
+++ doc/astro.config.ts
@@ -1,8 +1,9 @@
 import mdx from "@astrojs/mdx";
+import solid from "@astrojs/solid-js";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
   base: "/monza-editor",
-  integrations: [mdx()],
+  integrations: [mdx(), solid()],
   site: "https://raviqqe.com/monza-editor",
 });
