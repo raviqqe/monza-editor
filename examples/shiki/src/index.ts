5baa0af066792677875af2cb4aae046affe20326
Yota Toyama <raviqqe@gmail.com>
2025-08-16 11:05:54 -0700

    Input event type (#33)

--- examples/shiki/src/index.ts
+++ examples/shiki/src/index.ts
@@ -26,5 +26,7 @@ initialize(editor, {
       structure: "inline",
       theme: "min-light",
     }),
+  // biome-ignore lint/suspicious/noConsole: Testing
+  onInput: (event) => console.log(event.target.value),
   value: `console.log("Hello, world!");`,
 });
