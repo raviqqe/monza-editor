c200296edda02b04531f1d4556fc51ca019daf0f
Yota Toyama <raviqqe@gmail.com>
2025-08-16 14:24:49 -0700

    Fix URL base (#37)

--- doc/astro.config.ts
+++ doc/astro.config.ts
@@ -2,5 +2,7 @@ import mdx from "@astrojs/mdx";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
+  base: "/monza-editor",
   integrations: [mdx()],
+  site: "https://raviqqe.com/monza-editor",
 });
