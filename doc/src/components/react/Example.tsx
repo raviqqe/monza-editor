df38168fd701433994577040c4e7a858d6daebb0
Yota Toyama <raviqqe@gmail.com>
2025-08-16 18:41:27 -0700

    Fix

---
+++ doc/src/components/react/Example.tsx
@@ -0,0 +1,30 @@
+import { Editor } from "@monza-editor/react";
+import { createHighlighterCore, createJavaScriptRegexEngine } from "shiki";
+import type { JSX } from "react";
+import { theme } from "../../utility.js";
+import styles from "../solid/Example.module.css";
+
+const highlighter = await createHighlighterCore({
+  engine: createJavaScriptRegexEngine(),
+  langs: [import("@shikijs/langs/typescript")],
+  themes: [
+    import("@shikijs/themes/github-dark"),
+    import("@shikijs/themes/github-light"),
+  ],
+});
+
+export const Example = (): JSX.Element => {
+  return (
+    <Editor
+      class={styles.main}
+      onHighlight={(text) =>
+        highlighter.codeToHtml(text, {
+          lang: "typescript",
+          structure: "inline",
+          theme,
+        })
+      }
+      value='console.log("Hello, Solid.js!");'
+    />
+  );
+};
