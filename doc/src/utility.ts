fad44184525bd563901dfae379aff73b0c324d2b
Yota Toyama <raviqqe@gmail.com>
2025-08-16 20:26:24 -0700

    Refactor example

--- doc/src/utility.ts
+++ doc/src/utility.ts
@@ -1,13 +1,5 @@
 import { createHighlighterCore, createJavaScriptRegexEngine } from "shiki";
 
-export const source = `
-const square = (x: number): number =>
-  x * x;
-
-console.log("Hello, world!");
-console.log(square(42));
-`.trim();
-
 export const theme =
   typeof window === "object" &&
   window.matchMedia("(prefers-color-scheme: dark)").matches
