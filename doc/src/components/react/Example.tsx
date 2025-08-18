db569d6059c1b722836cbfd74e84e7a342736a3f
Yota Toyama <raviqqe@gmail.com>
2025-08-17 17:54:58 -0700

    Fix

--- doc/src/components/react/Example.tsx
+++ doc/src/components/react/Example.tsx
@@ -1,19 +1,13 @@
 import { Editor } from "@monza-editor/react";
 import type { JSX } from "react";
-import { highlighter, theme } from "../../utility.js";
+import { highlight } from "../../utility.js";
 import styles from "./Example.module.css";
 
 export const Example = (): JSX.Element => {
   return (
     <Editor
       className={styles.main}
-      onHighlight={(text) =>
-        highlighter.codeToHtml(text, {
-          lang: "typescript",
-          structure: "inline",
-          theme,
-        })
-      }
+      onHighlight={highlight}
       value='console.log("Hello, React!");'
     />
   );
