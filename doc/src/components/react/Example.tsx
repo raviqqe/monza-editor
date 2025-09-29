749ecd2c046eb90c14a8239ed1412857c303c68f
Yota Toyama <raviqqe@gmail.com>
2025-09-29 08:25:14 -0700

    Fix

--- doc/src/components/react/Example.tsx
+++ doc/src/components/react/Example.tsx
@@ -1,13 +1,18 @@
 import "monza-editor/style.css";
 import { Editor } from "@monza-editor/react";
-import type { JSX } from "react";
+import { type JSX, useState } from "react";
 import { highlight } from "../../utility.js";
 import styles from "./Example.module.css";
 
-export const Example = (): JSX.Element => (
-  <Editor
-    className={styles.main}
-    onHighlight={highlight}
-    value='console.log("Hello, React!");'
-  />
-);
+export const Example = (): JSX.Element => {
+  const [value, setValue] = useState('console.log("Hello, React!");');
+
+  return (
+    <Editor
+      className={styles.main}
+      onChange={({ target }) => setValue(target.value)}
+      onHighlight={highlight}
+      value={value}
+    />
+  );
+};
